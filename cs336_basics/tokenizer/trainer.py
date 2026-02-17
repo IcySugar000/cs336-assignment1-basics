8f36794a673c026ffe88161669bba1300ad423dc
IcySugar <74488557+IcySugar000@users.noreply.github.com>
2026-02-17 18:28:02 +0800

    修复tokenizer（尚未修复owt数据）

--- cs336_basics/tokenizer/trainer.py
+++ cs336_basics/tokenizer/trainer.py
@@ -81,6 +81,7 @@ class MaxHeapItem:
 class Trainer:
     vocab: dict[int, bytes]
     vocab_size: int
+    vocab_values: set[bytes]
     merges: list[tuple[bytes, bytes]]
     special_tokens: list[str]
 
@@ -101,6 +102,7 @@ class Trainer:
         self.vocab = {i: i.to_bytes() for i in range(256)}
         for i in range(len(special_tokens)):
             self.vocab[256 + i] = special_tokens[i].encode()
+        self.vocab_values = set(self.vocab.values())
 
         # 2. 初始化成员变量
         self.merges = []
@@ -188,7 +190,10 @@ class Trainer:
             self.merges.append(max_item[0])
 
             new_vocab = max_item[0][0] + max_item[0][1]
+            if new_vocab in self.vocab_values:
+                continue
             self.vocab[len(self.vocab)] = new_vocab
+            self.vocab_values.add(new_vocab)
             self.token_to_words[new_vocab] = set()
 
             for word in self.token_to_words[max_item[0][0]].intersection(self.token_to_words[max_item[0][1]]):
@@ -202,15 +207,19 @@ class Trainer:
                     end = start + 1
 
                     find = True
-                    replacing_idx.append(start)
                     if start != 0:
-                        pair = (word[start - 1], word[start])
+                        # 在形如"ssss"的情况时，合并s+s时，由于word未更新，会导致合并后两个s时word[start-1]依旧为s
+                        # 因此，如果上一个token已经被合并，此时应该用合并后的结果取代word[start-1]
+                        if replacing_idx and start == replacing_idx[-1] + 2:
+                            pair = (new_vocab, word[start])
+                        else:
+                            pair = (word[start - 1], word[start])
                         if pairs.get(pair):
                             pairs[pair] -= self.words[word]
                             heapq.heappush(heap, MaxHeapItem((pair, pairs[pair])))
                             if not pairs[pair]:
                                 pairs.pop(pair)
-                        pair = (word[start - 1], new_vocab)
+                        pair = (pair[0], new_vocab)
                         pairs[pair] = pairs.get(pair, 0) + self.words[word]
                         heapq.heappush(heap, MaxHeapItem((pair, pairs[pair])))
                     if end != len(word) - 1:
@@ -224,6 +233,7 @@ class Trainer:
                         pairs[pair] = pairs.get(pair, 0) + self.words[word]
                         heapq.heappush(heap, MaxHeapItem((pair, pairs[pair])))
 
+                    replacing_idx.append(start)
                     start += 2
 
                 if find:
@@ -244,19 +254,18 @@ class Trainer:
                     for token in set(new_word):
                         self.token_to_words[token].add(new_word_tuple)
 
+    def save(self, name: str):
+        encoder = gpt2_bytes_to_unicode()
+        encoded_merges = []
+        for merge in self.merges:
+            encoded_merges.append(f"{''.join(encoder[b] for b in merge[0])} {''.join(encoder[b] for b in merge[1])}")
+        with open(f"checkpoints/tokenizer/{name}_vocab.json", mode="w", encoding="utf-8") as f:
+            json.dump({v.decode("latin1"): k for k, v in self.vocab.items()}, f)
+        with open(f"checkpoints/tokenizer/{name}_merges.txt", mode="w", encoding="utf-8") as f:
+            f.write("\n".join(encoded_merges))
+
 
 if __name__ == "__main__":
-    # tokenizer = Trainer("data/TinyStoriesV2-GPT4-train.txt", 10000, ["<|endoftext|>"], num_processes=16, num_splits=16)
-    tokenizer = Trainer("data/owt_train.txt", 32000, ["\n"], num_processes=16, num_splits=64)
-
-    # save
-    vocab, merges = tokenizer.vocab, tokenizer.merges
-    encoder = gpt2_bytes_to_unicode()
-    encoded_vocab, encoded_merges = {}, []
-    for key, value in vocab.items():
-        new_value = "".join([encoder[b] for b in value])
-        encoded_vocab[key] = new_value
-    for merge in merges:
-        encoded_merges.append(("".join(encoder[b] for b in merge[0]), "".join(encoder[b] for b in merge[1])))
-    with open("checkpoints/tokenizer/owt.json", "w", encoding="utf-8") as f:
-        json.dump({"vocab": encoded_vocab, "merges": encoded_merges}, f, ensure_ascii=False, indent=4)
+    dataset = "owt"
+    tokenizer = Trainer(f"data/{dataset}_train.txt", 32000, ["<|endoftext|>"], num_processes=16, num_splits=256)
+    tokenizer.save(dataset)
